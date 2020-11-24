dd058c68319847257cc09bd5d3b2c78ce9d1a24a
mikg0 <mikg0@users.noreply.github.com>
2020-11-24 17:27:33 +0100

    Create bookshop_sqlite.sql
    make orders work in app, when using sqlite

---
+++ db/bookshop_sqlite.sql
@@ -0,0 +1,22 @@
+create table book (
+  isbn    text not null primary key,
+  title   text not null,
+  author  text not null,
+  price   decimal(10,2) not null
+);
+
+create table custorder (
+  id      int primary key,
+  name    text not null,
+  street  text not null,
+  city    text not null
+);
+
+create table custorderitem (
+  custorderid int not null,
+  isbn    text not null,
+  amount  int not null,
+  primary key (custorderid, isbn),
+  foreign key (custorderid) references custorder,
+  foreign key (isbn) references book
+);
